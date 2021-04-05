264e316d1f48548fdf788f14ff5dff5664bbbf62
Dwiki Wahyudi <dwikiwahyudi77@gmail.com>
2021-04-05 14:20:10 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores optio dolorem a. Sit magnam dolores consequatur ratione eum laudantium pariatur voluptate doloremque excepturi, ipsum rerum consequuntur blanditiis laborum esse tempora!</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
 </body>
 </html>
